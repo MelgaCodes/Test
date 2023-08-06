68d73166b475e03cd1655fd2a324a2d29d9e33ad
alvaro <tireempuje@gmail.com>
2023-08-06 22:42:30 +0200

    Made the words appear in a random order instead of the same order every time the program is executed

--- src/gamelogic.py
+++ src/gamelogic.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 
 def game(lang, topic):
@@ -10,10 +11,14 @@ def game(lang, topic):
         data = json.load(file)
 
     the_list = list(data[f"{topic}"])
+    original_len_list = len(the_list)
 
-    for item in the_list:
-        local_dict = data[f"{topic}"][item]
-        user_choice = input("What is the translation of " + item + "\n")
+    for i in range(0,len(the_list)):
+        rand_item = random.choice(the_list)
+        local_dict = data[f"{topic}"][rand_item]
+        user_choice = input("What is the translation of " + rand_item + "\n")
+        the_list.remove(rand_item)
+
         if user_choice == local_dict["translation"][lang]:
             print("That is correct!\n")
             score += 1
@@ -21,7 +26,7 @@ def game(lang, topic):
         else:
             print("That is not correct\n")
 
-    if score == len(the_list):
+    if score == original_len_list:
         perfect_score = True
 
     return [score, perfect_score]
