9ee34e29d725c66144a3b194eb7254fff1f7de78
alvaro <tireempuje@gmail.com>
2023-08-06 22:34:08 +0200

    Expanded dataset

--- src/cli.py
+++ src/cli.py
@@ -7,8 +7,8 @@ topic = ""
 while lang != "spanish" and lang != "polish":
     lang = input("To what language do you want to translate?\n")
 
-while topic != "fruit" and topic != "transport":
-    topic = input("What category do you want to play: fruit or transport?\n")
+while topic != "fruit" and topic != "transport" and topic != "clothes":
+    topic = input("What category do you want to play: fruit, transport, clothes?\n")
 
 results = game(lang, topic)
 score = results[0]
